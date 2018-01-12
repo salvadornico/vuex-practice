47d76df00fc4d8b5aec0f217085bd6d2152555ea
Nico <salvador.nico@gmail.com>
2018-01-12 21:32:14 +0800

    After video #2

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,31 +1,27 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parser: "vue-eslint-parser",
-  parserOptions: {
-    "parser": "babel-eslint",
-    "ecmaVersion": 2017,
-    sourceType: "module"
-  },
-  env: {
-    browser: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:vue/recommended"
-  ],
-  // required to lint *.vue files
-  plugins: [
-    "html"
-  ],
-  "rules": {
-    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
-    "arrow-parens": 0,
-    "generator-star-spacing": 0,
-    "indent": ["error", "tab"],
-    "quotes": ["error", "double"],
-    "linebreak-style": ["warn", "unix"],
-    "no-undef": "off"
-  }
-}
+	root: true,
+	parser: "vue-eslint-parser",
+	parserOptions: {
+		parser: "babel-eslint",
+		ecmaVersion: 2017,
+		sourceType: "module"
+	},
+	env: {
+		browser: true
+	},
+	extends: ["eslint:recommended", "plugin:vue/recommended"],
+	// required to lint *.vue files
+	plugins: ["html"],
+	rules: {
+		"no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+		"arrow-parens": 0,
+		"generator-star-spacing": 0,
+		indent: ["error", "tab"],
+		quotes: ["error", "double"],
+		"linebreak-style": ["warn", "unix"],
+		"no-undef": "off",
+		"vue/require-v-for-key": "off"
+	}
+};
